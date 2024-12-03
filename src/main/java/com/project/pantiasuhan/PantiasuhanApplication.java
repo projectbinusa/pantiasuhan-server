9e9706f3c269b7163655f783d8cb4593ef3c9131
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-12-03 14:03:15 +0700

    first commit

---
+++ src/main/java/com/project/pantiasuhan/PantiasuhanApplication.java
@@ -0,0 +1,13 @@
+package com.project.pantiasuhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantiasuhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantiasuhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pantiasuhan/PantiasuhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pantiasuhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantiasuhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
